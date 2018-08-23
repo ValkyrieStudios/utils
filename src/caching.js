ebef5cc51b26e7dd73aad7aadc930c10453733fa
Peter Vermeulen <contact@valkyriestudios.be>
2018-08-23 02:05:45 +0200

    Add testcase for caching utils

--- src/caching.js
+++ src/caching.js
@@ -1,23 +1,13 @@
-import {fnv1A} from './hash';
-
 export function memoize (fn) {
-    return function () {
-        const args = Array.prototype.slice.call(arguments);
-        let hash = fnv1A(args);
+    const cache = {};
 
-        //  Set memoization cache on the function if it doesn't exist yet
-        fn.memoize || (fn.memoize = Object.create(null));
+    return function memoized (...args) {
+        const key = JSON.stringify(args);
 
-        //  Check if the map contains our hash as a key
-        //  true --> return the value associated with the hash immediately
-        //  false --> make a function call and store the result in the map, then return the result
-        if (fn.memoize[hash]) {
-            return fn.memoize[hash];
+        if (key in cache) {
+            return cache[key];
         }
 
-        const result = fn.apply(this, args);
-
-        fn.memoize[hash] = result;
-        return result;
+        return (cache[key] = (fn(...args)));
     };
 };
---
+++ test/src/caching.test.js
@@ -0,0 +1,49 @@
+import { memoize } from '../../src/caching';
+import { fnv1A } from '../../src/hash';
+
+describe("Caching - memoize", () => {
+    let rslt_uncached, rslt_cached;
+
+    beforeEach(() => {
+        function fn (a, b, c) {
+            return fnv1A(a + b + c);
+        }
+
+        const testcase = [
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+        ];
+
+        //  Uncached
+        const test_uncached = function () {
+            rslt_uncached = new Date();
+            for (let i = 0; i < 100000; i++) {
+                fn(...testcase[Math.floor(Math.random() * (6 - 0 + 1)) + 0]);
+            }
+            rslt_uncached = new Date() - rslt_uncached;
+        };
+
+        const test_cached = function () {
+            fn = memoize(fn);
+
+            //  Cached
+            rslt_cached = new Date();
+            for (let i = 0; i < 100000; i++) {
+                fn(...testcase[Math.floor(Math.random() * (6 - 0 + 1)) + 0]);
+            }
+            rslt_cached = new Date() - rslt_cached;
+        };
+
+        test_uncached();
+        test_cached();
+    });
+
+    it ('should cache', () => {
+        expect(rslt_cached < rslt_uncached).toBe(true);
+    });
+});

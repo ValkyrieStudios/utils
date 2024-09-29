ece25cb00ed11b7ce4331fe203c879bf9506b0fa
Peter Vermeulen <contact@valkyriestudios.be>
2024-09-29 18:56:51 +0200

    [feat] Add convertToDate

---
+++ lib/date/convertToDate.ts
@@ -0,0 +1,17 @@
+import {isDate} from './is';
+
+/**
+ * Convert a value to a Date object if possible, handles Date objects and strings
+ *
+ * @param {Date|string} val - Value to convert
+ */
+export function convertToDate (val: Date | string | number): Date | null {
+    if (isDate(val)) {
+        return val;
+    } else if (typeof val === 'string') {
+        const date = new Date(val);
+        return Number.isNaN(date.getTime()) ? null : date;
+    }
+
+    return null;
+}

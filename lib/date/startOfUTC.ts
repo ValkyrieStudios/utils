3eec39ef8f9f8f4d7b02b027a24d7854b53de783
Peter Vermeulen <contact@valkyriestudios.be>
2024-09-29 15:12:22 +0200

    [feat] Add support for date strings in date/startOfUTC

--- lib/date/startOfUTC.ts
+++ lib/date/startOfUTC.ts
@@ -1,4 +1,4 @@
-import {isDate} from './is';
+import {convertToDate} from './convertToDate';
 
 export type StartOfUTCKey = 'year'
     | 'quarter'
@@ -31,48 +31,27 @@ const WEEK_START = new Map([
 /**
  * Sets the provided date to start of UTC of provided key
  *
- * @param {Date} val - Date to set to start of
+ * @param {Date|string} val - Date to set to start of
  * @param {StartOfUTCKey} key - (default='millisecond') Key to set
  */
 function startOfUTC (
-    val:Date,
+    val:Date|string,
     key:StartOfUTCKey = 'millisecond'
 ):Date {
-    if (!isDate(val)) throw new TypeError('startOfUTC requires a date object');
+    const n_val = convertToDate(val);
+    if (n_val === null) throw new TypeError('startOfUTC requires a date object');
+
+    const year = n_val.getUTCFullYear();
 
     switch (key) {
         case 'year':
-            return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                0,
-                1,
-                0,
-                0,
-                0,
-                0
-            ));
+            return new Date(Date.UTC(year, 0));
         case 'quarter': {
-            const UTC_MONTH = val.getUTCMonth();
-            return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                UTC_MONTH - (UTC_MONTH % 3),
-                1,
-                0,
-                0,
-                0,
-                0
-            ));
+            const UTC_MONTH = n_val.getUTCMonth();
+            return new Date(Date.UTC(year, UTC_MONTH - (UTC_MONTH % 3)));
         }
         case 'month':
-            return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                1,
-                0,
-                0,
-                0,
-                0
-            ));
+            return new Date(Date.UTC(year, n_val.getUTCMonth()));
         case 'week':
         case 'week_sun':
         case 'week_mon':
@@ -81,67 +60,53 @@ function startOfUTC (
         case 'week_thu':
         case 'week_fri':
         case 'week_sat': {
-            const UTC_DAY = val.getUTCDay();
+            const UTC_DAY = n_val.getUTCDay();
             const UTC_SOD = WEEK_START.get(key) as number;
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate() - (UTC_DAY < UTC_SOD ? (7 - UTC_SOD) + UTC_DAY : UTC_DAY - UTC_SOD),
-                0,
-                0,
-                0,
-                0
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate() - (UTC_DAY < UTC_SOD ? (7 - UTC_SOD) + UTC_DAY : UTC_DAY - UTC_SOD)
             ));
         }
         case 'day':
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate(),
-                0,
-                0,
-                0,
-                0
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate()
             ));
         case 'hour':
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate(),
-                val.getUTCHours(),
-                0,
-                0,
-                0
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate(),
+                n_val.getUTCHours()
             ));
         case 'minute':
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate(),
-                val.getUTCHours(),
-                val.getUTCMinutes(),
-                0,
-                0
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate(),
+                n_val.getUTCHours(),
+                n_val.getUTCMinutes()
             ));
         case 'second':
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate(),
-                val.getUTCHours(),
-                val.getUTCMinutes(),
-                val.getUTCSeconds(),
-                0
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate(),
+                n_val.getUTCHours(),
+                n_val.getUTCMinutes(),
+                n_val.getUTCSeconds()
             ));
         default:
             return new Date(Date.UTC(
-                val.getUTCFullYear(),
-                val.getUTCMonth(),
-                val.getUTCDate(),
-                val.getUTCHours(),
-                val.getUTCMinutes(),
-                val.getUTCSeconds(),
-                val.getUTCMilliseconds()
+                year,
+                n_val.getUTCMonth(),
+                n_val.getUTCDate(),
+                n_val.getUTCHours(),
+                n_val.getUTCMinutes(),
+                n_val.getUTCSeconds(),
+                n_val.getUTCMilliseconds()
             ));
     }
 }

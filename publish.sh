faa6ae28e98502a2ef8a33bb6cba892e52ff5f6a
Peter Vermeulen <contact@valkyriestudios.be>
2025-02-04 16:35:39 +0100

    [ops] Remove jq behavior during publishing

--- publish.sh
+++ publish.sh
@@ -13,11 +13,6 @@ npx tsc -p ./tsconfig.types_all.json
 # Copy files over
 cp package.json README.md LICENSE.md .npmignore ./dist
 
-# Edit package.json to remove scripts/devDependencies
-echo $(cat ./dist/package.json | jq 'del(.scripts)') > ./dist/package.json
-echo $(cat ./dist/package.json | jq 'del(.devDependencies)') > ./dist/package.json
-echo $(cat ./dist/package.json | jq '.+={types:"index.d.ts"}') > ./dist/package.json
-
 cd ./dist
 
 npm publish
